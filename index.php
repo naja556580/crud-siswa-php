3a6285405979ea7a0b3452513848bed226b9029c
Rifky <najarifky556@gmail.com>
2026-05-06 12:42:13 +0700

    Add 'Jurusan' column to siswa data table

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
-<?php
+php<?php
 $koneksi = mysqli_connect("localhost", "root", "", "crud_tugas");
 $result = mysqli_query($koneksi, "SELECT * FROM siswa");
+$no = 1;
 ?>
 
 <h2>Data Siswa</h2>
 <a href="tambah.php">Tambah Data</a>
 <br><br>
-<table border="1" cellpadding="10">
+<table border="1" cellpadding="10" cellspacing="0">
     <tr>
         <th>No</th>
         <th>Nama</th>
         <th>Kelas</th>
+        <th>Jurusan</th>
         <th>Aksi</th>
     </tr>
-    <?php $i = 1; ?>
-    <?php while($row = mysqli_fetch_assoc($result)) : ?>
+    <?php while($row = mysqli_fetch_assoc($result)) :?>
     <tr>
-        <td><?= $i; ?></td>
-        <td><?= $row["nama"]; ?></td>
-        <td><?= $row["kelas"]; ?></td>
+        <td><?= $no++?></td>
+        <td><?= $row['nama']?></td>
+        <td><?= $row['kelas']?></td>
+        <td><?= $row['jurusan']?></td>
         <td>
-            <a href="edit.php?id=<?= $row["id"]; ?>">Edit</a> | 
-            <a href="hapus.php?id=<?= $row["id"]; ?>">Hapus</a>
+            <a href="edit.php?id=<?= $row['id']?>">Edit</a> |
+            <a href="hapus.php?id=<?= $row['id']?>">Hapus</a>
         </td>
     </tr>
-    <?php $i++; ?>
-    <?php endwhile; ?>
+    <?php endwhile;?>
 </table>
