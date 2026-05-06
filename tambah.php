79d23ad41eeda67a26ace58add928f51199efcd7
Rifky <najarifky556@gmail.com>
2026-05-06 12:43:55 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -4,9 +4,15 @@ $koneksi = mysqli_connect("localhost", "root", "", "crud_tugas");
 if(isset($_POST["submit"])) {
     $nama = $_POST["nama"];
     $kelas = $_POST["kelas"];
+    $jurusan = $_POST["jurusan"];
     
-    mysqli_query($koneksi, "INSERT INTO siswa VALUES('', '$nama', '$kelas')");
-    echo "<script>alert('Data berhasil ditambah');document.location.href='index.php';</script>";
+    $query = mysqli_query($koneksi, "INSERT INTO siswa (nama, kelas, jurusan) VALUES ('$nama', '$kelas', '$jurusan')");
+    
+    if(!$query){
+        die("Error: " . mysqli_error($koneksi));
+    } else {
+        echo "<script>alert('Data berhasil ditambah');document.location.href='index.php'</script>";
+    }
 }
 ?>
 
@@ -14,6 +20,7 @@ if(isset($_POST["submit"])) {
 <form action="" method="post">
     Nama: <input type="text" name="nama" required><br><br>
     Kelas: <input type="text" name="kelas" required><br><br>
+    Jurusan: <input type="text" name="jurusan" required><br><br>
     <button type="submit" name="submit">Tambah Data</button>
 </form>
 <br>
